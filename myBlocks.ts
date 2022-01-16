0df5fc102d7e5b23e60765c3fd74812ce84f6adc
Mitsuyoshi Otani <mi.forwork0123@gmail.com>
2022-01-16 11:30:51 +0900

    ver 0.0.0

---
+++ myBlocks.ts
@@ -0,0 +1,32 @@
+enum TempConvertType{
+    //% block= "°C → °F"
+    CtoF,
+    //% block= "°F → °C"
+    FtoC,
+}
+
+//% color=#2bd9be weight=95 icon="uf0ad"
+namespace myBlocks {
+    //% blockId=myBlocks_convertTemperature
+    //% block="convert|%value|%convertType"
+    export function convertTemperature(value: number, convertType: TempConvertType): number{
+        if (convertType === TempConvertType.CtoF) {
+            return cToF(value);
+        }
+        if (convertType === TempConvertType.FtoC) {
+            return fToC(value);
+        }
+        return value;
+    }
+
+    //% shim=myBlocks::cToF
+    function cToF(value: number) {
+        return 32.0;
+    }
+
+    //% shim=myBlocks::fToC
+    function fToC(value: number) {
+        return -17.78;
+    }
+    
+}
